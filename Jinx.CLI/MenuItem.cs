ef75480bfce7c4c6fbbc1db348cc9988e495852b
Fusion86 <fusion86@cerbus.nl>
2018-05-17 14:41:21 +0200

    Update

---
+++ Jinx.CLI/MenuItem.cs
@@ -0,0 +1,27 @@
+﻿using Jinx.Core.Abilities;
+using System;
+using System.Threading.Tasks;
+
+namespace Jinx.CLI
+{
+    class MenuItem
+    {
+        public string Name;
+        public bool IsAbility => Ability != null;
+
+        public AbilityBase Ability { get; private set; }
+        public Func<Task> Run { get; private set; }
+
+        public MenuItem(AbilityBase ability)
+        {
+            Name = ability.Name;
+            Ability = ability;
+        }
+
+        public MenuItem(string name, Func<Task> run)
+        {
+            Name = name;
+            Run = run;
+        }
+    }
+}
--- Jinx.CLI/Program.cs
+++ Jinx.CLI/Program.cs
@@ -1,61 +1,110 @@
 ﻿using Hextech.LeagueClient;
+using Hextech.LeagueClient.Models.System;
 using Jinx.Core;
 using Jinx.Core.Abilities;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Reflection;
+using System.Threading.Tasks;
 
 namespace Jinx.CLI
 {
     class Program
     {
-        static readonly Dictionary<ConsoleKey, AbilityBase> menuItems = new Dictionary<ConsoleKey, AbilityBase>();
+        static readonly Dictionary<ConsoleKey, MenuItem> menuItems = new Dictionary<ConsoleKey, MenuItem>();
 
         static readonly LeagueClientApi lc = new LeagueClientApi();
 
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
             // Register abilities
-            menuItems.Add(ConsoleKey.A, new AutoAcceptAbility());
+            menuItems.Add(ConsoleKey.A, new MenuItem(new AutoAcceptAbility()));
 
-            MainMenu();
-        }
+            // Register action items
+            menuItems.Add(ConsoleKey.B, new MenuItem("Print BuildInfo", async () =>
+            {
+                var info = await lc.System.GetBuildInfo();
+                Console.WriteLine(JsonConvert.SerializeObject(info, Formatting.Indented));
+            }));
 
-        static void MainMenu()
-        {
-        menu:
-
-            Console.Clear();
             Console.WriteLine($"Jinx v{Assembly.GetExecutingAssembly().GetName().Version} (Core v{Assembly.GetAssembly(typeof(AbilityBase)).GetName().Version})\n");
 
-            Console.WriteLine();
-            Console.Write("C - Connect to the LeagueClient");
-            Console.WriteLine(lc.IsConnected ? " [ Connected ]" : " [ Not Connected ]");
-            Console.WriteLine();
+            Console.WriteLine("Trying to connect to the LeagueClient...");
+            bool success = await lc.Initialize();
 
-            foreach (var item in menuItems)
+            if (success)
             {
-                Console.Write(item.Key + " - " + item.Value.Name);
-                Console.WriteLine(item.Value.IsEnabled ? " [ Enabled ]" : " [ Disabled ]");
+                Console.WriteLine("Connected to the LeagueClient");
+                Console.WriteLine("Requestiong LeagueClient version...");
+                BuildInfo info = await lc.System.GetBuildInfo();
+                Console.WriteLine("LeagueClient version is " + info.Version);
+                Console.WriteLine();
+
+                await MainMenuLoop();
             }
-
-            Console.WriteLine();
-            Console.Write("> ");
-
-            ConsoleKey key = Console.ReadKey().Key;
-
-            AbilityBase selectedAbility = null;
-            menuItems.TryGetValue(key, out selectedAbility);
-
-            if (selectedAbility != null)
+            else
             {
-                if (selectedAbility.IsEnabled) selectedAbility.Disable();
-                else selectedAbility.Enable();
+                Console.WriteLine("Couldn't connect to the LeagueClient");
+                Console.WriteLine("Exiting...");
             }
-            else if (key == ConsoleKey.C) lc.Initialize().Wait();
-            else if (key == ConsoleKey.Q) return;
+        }
 
-            goto menu;
+        static async Task MainMenuLoop()
+        {
+            bool first = true;
+            var actionItems = menuItems.Where(x => x.Value.IsAbility == false);
+            var abilityItems = menuItems.Where(x => x.Value.IsAbility == true);
+
+            while (true)
+            {
+                if (first) first = false;
+                else Console.Clear();
+
+                foreach (var item in actionItems)
+                {
+                    Console.WriteLine(item.Key + " - " + item.Value.Name);
+                }
+
+                if (actionItems.Count() > 0) Console.WriteLine();
+
+                foreach (var item in abilityItems)
+                {
+                    Console.Write(item.Key + " - " + item.Value.Name);
+
+                    ConsoleColor oldColor = Console.ForegroundColor;
+                    Console.ForegroundColor = item.Value.Ability.IsEnabled ? ConsoleColor.Green : ConsoleColor.Yellow;
+                    Console.WriteLine(item.Value.Ability.IsEnabled ? " [ Enabled ]" : " [ Disabled ]");
+                    Console.ForegroundColor = oldColor;
+                }
+
+                if (abilityItems.Count() > 0) Console.WriteLine();
+
+                Console.Write("> ");
+
+                ConsoleKey key = Console.ReadKey().Key;
+
+                MenuItem selectedItem = null;
+                menuItems.TryGetValue(key, out selectedItem);
+
+                if (selectedItem != null)
+                {
+                    if (selectedItem.IsAbility)
+                    {
+                        if (selectedItem.Ability.IsEnabled) selectedItem.Ability.Disable();
+                        else selectedItem.Ability.Enable();
+                    }
+                    else
+                    {
+                        Console.Clear();
+                        await selectedItem.Run();
+                        Console.WriteLine("\nPress enter to go back to the main menu...");
+                        Console.ReadLine();
+                    }
+                }
+                else if (key == ConsoleKey.Q) return;
+            }
         }
     }
 }
