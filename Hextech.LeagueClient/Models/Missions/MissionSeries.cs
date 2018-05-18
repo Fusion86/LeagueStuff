62d68101792cbc92564205a3c1483ee55640d243
Fusion86 <fusion86@cerbus.nl>
2018-05-18 22:31:11 +0200

    Add EpochConverter

--- Hextech.LeagueClient/Models/Missions/MissionSeries.cs
+++ Hextech.LeagueClient/Models/Missions/MissionSeries.cs
@@ -1,28 +1,39 @@
+using Hextech.LeagueClient.JsonConverters;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 
 namespace Hextech.LeagueClient.Models.Missions
 {
     public class MissionSeries
     {
-        public long CreatedDate { get; set; }
         public string Description { get; set; }
         public string DisplayType { get; set; }
         public string EligibilityType { get; set; }
-        public long EndDate { get; set; }
         public string Id { get; set; }
         public string InternalName { get; set; }
-        public long LastUpdatedTimestamp { get; set; }
         public MissionSeriesMedia Media { get; set; }
         public string OptInButtonText { get; set; }
         public string OptOutButtonText { get; set; }
         public string ParentName { get; set; }
-        public long StartDate { get; set; }
         public string Status { get; set; }
         public List<string> Tags { get; set; }
         public string Title { get; set; }
         public string Type { get; set; }
         public bool Viewed { get; set; }
         public List<Alert> Warnings { get; set; }
+
+        [JsonConverter(typeof(EpochConverter))]
+        public DateTime LastUpdatedTimestamp { get; set; }
+
+        [JsonConverter(typeof(EpochConverter))]
+        public DateTime CreatedDate { get; set; }
+
+        [JsonConverter(typeof(EpochConverter))]
+        public DateTime StartDate { get; set; }
+
+        [JsonConverter(typeof(EpochConverter))]
+        public DateTime EndDate { get; set; }
     }
 
     public class MissionSeriesMedia
